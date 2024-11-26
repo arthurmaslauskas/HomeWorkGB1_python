a0ad924fc017d9f9fdaebeb2e99cdd5a0b625efa
Arthur Maslauskas <mickeyarthurfirst@gmail.com>
2024-11-26 13:22:19 +1100

    Point 4

--- HomeWork1GB.py
+++ HomeWork1GB.py
@@ -1,13 +1,22 @@
 #Point 2
-def kartesh(num):
-    num_str =str (num)
-    sum_kartesh = sum(int(kartesh) for kartesh in num_str)
-    return sum_kartesh
+# def kartesh(num):
+#     num_str =str (num)
+#     sum_kartesh = sum(int(kartesh) for kartesh in num_str)
+#     return sum_kartesh
 
-number = int(input("Write number's three num: "))
+# number = int(input("Write number's three num: "))
 
-if 100 <= number <= 999:
-    result = kartesh(number)
-    print(f"Total numb's {number}: {result}")
-else:
-    print("Write three number's")
+# if 100 <= number <= 999:
+#     result = kartesh(number)
+#     print(f"Total numb's {number}: {result}")
+# else:
+#     print("Write three number's")
+
+def find_cranes(num):
+    x = num // 4
+    return (x, 2*x, x)
+
+num = int(input("Write numbers (num): "))
+
+result = find_cranes(num)
+print(f" Petya complite {result}, Katya complite {result}, Sergey complite {result}")
