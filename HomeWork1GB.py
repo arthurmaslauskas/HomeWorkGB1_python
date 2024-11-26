9c49cbe5840d0fe6c26ab2f94da9e7c4b83778e2
Arthur Maslauskas <mickeyarthurfirst@gmail.com>
2024-11-26 13:49:23 +1100

    Point 6

--- HomeWork1GB.py
+++ HomeWork1GB.py
@@ -12,11 +12,30 @@
 # else:
 #     print("Write three number's")
 
-def find_cranes(num):
-    x = num // 4
-    return (x, 2*x, x)
+# Point 4
+# def find_cranes(num):
+#     x = num // 4
+#     return (x, 2*x, x)
 
-num = int(input("Write numbers (num): "))
+# num = int(input("Write numbers (num): "))
 
-result = find_cranes(num)
-print(f" Petya complite {result}, Katya complite {result}, Sergey complite {result}")
+# result = find_cranes(num)
+# print(f" Petya complite {result}, Katya complite {result}, Sergey complite {result}")
+
+def billet_point(ticket_number):
+    ticket_str = str(ticket_number)
+    ticket_lenght = len(ticket_str) // 2
+    
+    first_ticket = sum(int(digit) for digit in ticket_str[:ticket_lenght])
+    two_ticket = sum(int(digit) for digit in ticket_str[ticket_lenght:])
+    return first_ticket == two_ticket
+
+
+ticket_user = int(input("Write your lucky number's: "))
+
+result = billet_point(ticket_user)
+
+if result:
+    print("Your WINNNER!")
+else:
+    print("Sorry, your LOOSSEE")
